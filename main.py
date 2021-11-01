0e28521437e2d90f7c69e117421e54f3c7b341d6
gakawarstone <pinnekerv@gmail.com>
2021-11-01 10:10:38 +0600

    make get privete method

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from notion.client import NotionClient
 from datetime import datetime
 import config
+from page import Page
 
 client = NotionClient(config.TOKEN)
 
-page = client.get_block("https://www.notion.so/gakawarstone/EL-DEDE-98997f76b28d48cb946d04e32b540e64")
+el_dede = Page("https://www.notion.so/gakawarstone/EL-DEDE-98997f76b28d48cb946d04e32b540e64")
+print(el_dede.__get())
 
-cv = client.get_collection_view("https://www.notion.so/gakawarstone/67f38400c29f4137ac285fe6569567e2?v=d7e870b67e4b4becb65132a814f8f7af")
-row = cv.collection.add_row()
-row.name = "Старый испанский монах"
-row.subject = "C#"
-row.deadline = datetime(2021, 10, 31, 17, 2)
-row.status = "Не начато"
 
 
 def main():
--- page.py
+++ page.py
@@ -1,6 +1,17 @@
+from notion.client import NotionClient
+import config
+
+client = NotionClient(config.TOKEN)
+
+
 class Page(object):
     def __init__(self, URL):
         self.URL = URL
+        self.block = self.__get()
 
     def get_URL(self):
         return self.URL
+
+    def __get(self, client=client):
+        """get NotionClient object"""
+        return client.get_block(self.URL)
