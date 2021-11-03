4b66a750a17faaf9794dcb359f6d498f3400177e
gakawarstone <pinnekerv@gmail.com>
2021-11-03 10:16:23 +0600

    fix and add command handler

--- bot.py
+++ bot.py
@@ -5,11 +5,14 @@ import bot_config as config
 class Bot(object):
     def __init__(self, TOKEN):
         self.__TOKEN = TOKEN
+        self.bot = self.__set_bot()
         self.dp = self.__set_dispatcher()
 
+    def __set_bot(self):
+        return aiogram.Bot(token=self.__TOKEN)
+
     def __set_dispatcher(self):
-        bot = aiogram.Bot(token=self.__TOKEN)
-        return aiogram.dispatcher.Dispatcher(bot)
+        return aiogram.dispatcher.Dispatcher(self.bot)
 
     def __add_message_handler(self, func):
         """
@@ -17,7 +20,16 @@ class Bot(object):
         """
         @self.dp.message_handler()
         async def handler(message: aiogram.types.Message):
-            await func(message)
+            func(message)
+
+    def __add_command_handler(self, command, func):
+        """
+        command - /<command> in telegram
+        func(message -> aiogram.types.Message)
+        """
+        @self.dp.message_handler(commands=[command])
+        async def handler(message: aiogram.types.Message):
+            func(message)
 
     def start(self):
         aiogram.utils.executor.start_polling(self.dp)
