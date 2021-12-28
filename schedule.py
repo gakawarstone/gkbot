fb61da44522719ded42d1021ed200f1e239be48a
gakawarstone <pinnekerv@gmail.com>
2021-12-28 09:46:03 +0600

    refactor

--- schedule.py
+++ schedule.py
@@ -18,21 +18,22 @@ class Schedule:
     def add_task(self, func):
         self.tasks.append(func)
 
+    async def scheduler(self):
+        while True:
+            print(datetime.datetime.now())
+            for text in data:
+                await bot.send_message(manage.admins[0], text)
+            await asyncio.sleep(5)
+
+    async def on_startup(self, dp):
+        asyncio.create_task(self.scheduler())
+
     def start(self):
         pass
 
 
-async def init(message):
-    print(message)
-    await message.answer('started from schedule check')
-    bot.add_state_handler(FSM.send_mes, send_mes)
-
-
-async def send_mes(message=None, text='test'):
-    await bot.send_message(manage.admins[0], text)
-
-
-async def set_sch(message: aiogram.types.Message):
+# bot handlers
+async def init(message: aiogram.types.Message):
     await message.answer('Write your remind name')
     bot.add_state_handler(FSM.get_name, get_name)
     await FSM.get_name.set()
@@ -45,24 +46,10 @@ async def get_name(message: aiogram.types.Message, state: FSMContext):
 
 
 class FSM(StatesGroup):
-    send_mes = State()
     get_name = State()
 
 
-# scheduel
-async def scheduler():
-    while True:
-        print(datetime.datetime.now())
-        for text in data:
-            await send_mes(text=text)
-        await asyncio.sleep(20)
-
-
-async def on_startup(dp):
-    asyncio.create_task(scheduler())
-
-
 if __name__ == '__main__':
-    bot.add_command_handler('sch', init)
-    bot.add_command_handler('set', set_sch)
-    bot.start(on_startup=on_startup)
+    sch = Schedule()
+    bot.add_command_handler('set', init)
+    bot.start(on_startup=sch.on_startup)
