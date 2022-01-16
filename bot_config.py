55c32c34e7a90cabb5844dc42bfd5d71fde9dea0
gakawarstone <pinnekerv@gmail.com>
2022-01-16 07:28:38 +0600

    add dotenv

--- bot_config.py
+++ bot_config.py
@@ -1,21 +1,20 @@
 from lib.bot import Bot
 from lib.schedule import Schedule
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # VARS
 IN_HEROKU = os.environ.get('IN_HEROKU')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+NOTION_TOKEN = os.environ.get('NOTION_TOKEN')
+NOTION_API_TOKEN = os.environ.get('NOTION_API_TOKEN')
+
 if IN_HEROKU:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    NOTION_TOKEN = os.environ.get('NOTION_TOKEN')
-    NOTION_API_TOKEN = os.environ.get('NOTION_API_TOKEN')
     # Heroku PostgreSQL server
     DATABASE_URL = os.environ.get('DATABASE_URL')
 else:
-    import env
-    BOT_TOKEN = env.BOT_TOKEN
-    NOTION_TOKEN = env.NOTION_TOKEN
-    NOTION_API_TOKEN = env.NOTION_API_TOKEN
-    LOCAL_DB_USER_PSWD = env.LOCAL_DB_USER_PSWD
+    LOCAL_DB_USER_PSWD = os.environ.get('LOCAL_DB_USER_PSWD').split()
 
 # main objects
 bot = Bot(BOT_TOKEN)
