6c05c95624654aed0898c31a0a1b4667d24865c3
gakawarstone <pinnekerv@gmail.com>
2022-01-22 17:33:23 +0600

    revert:  drop class Common

--- models/common.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import declarative_base
-
-from bot_config import Session
-
-Base = declarative_base()
-
-
-class Common:
-    session = Session()
--- models/road.py
+++ models/road.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, Integer
 from sqlalchemy.orm import declarative_base
 
-from .common import Common
-
 Base = declarative_base()
 
 
-class PomodoroStats(Base, Common):
+class PomodoroStats(Base):
     __tablename__ = 'pomodoro'
 
     user_id = Column(Integer, primary_key=True)
@@ -15,15 +13,3 @@ class PomodoroStats(Base, Common):
 
     def __repr__(self):
         return f'<PomodoroStats(user_id={self.user_id})>'
-
-    @classmethod
-    def get_user(cls, user_id: int) -> object:
-        return cls.session.query(cls).filter_by(
-            user_id=user_id).first()
-
-    def increment(self, column: str) -> None:
-        column = self.__getattribute__(column)
-        print(column)
-        column += 1
-        PomodoroStats.session.commit()
-        pass
