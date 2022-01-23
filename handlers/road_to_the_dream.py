ebdab29ec0ba08769fb316413199646f8d7d2f31
gakawarstone <pinnekerv@gmail.com>
2022-01-23 08:00:12 +0600

    style: fix long lines

--- handlers/road_to_the_dream.py
+++ handlers/road_to_the_dream.py
@@ -1,24 +1,23 @@
 import asyncio
 from datetime import datetime
-from pprint import pprint
 
 import aiogram
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardRemove
 
+from bot_config import bot, Session
 from models.road import PomodoroStats
-from bot_config import bot, engine
-from sqlalchemy.orm import sessionmaker
 
 data = {'msg_if_restart': None}
 
 
 async def start(message: aiogram.types.Message):
     photo_id = 'AgACAgIAAxkDAALtVWHn3ZZmzpMfA3SI7usT1avw9xrWAALRtjEbe9FASzJZxPBxsVhdAQADAgADeQADIwQ'
-    await message.answer_photo(photo_id,
-                               caption='Привет <i>%s</i> ты включил модуль 🚀<b>РОД ЗЕ ДРИМ</b>🚀' %
-                               message['from']['first_name'])
+    await message.answer_photo(
+        photo_id,
+        caption='Привет <i>%s</i> ты включил модуль 🚀<b>РОД ЗЕ ДРИМ</b>🚀' %
+        message['from']['first_name'])
     buttons = [['Помидор 🕔', 'Трекер привычек']]
     bot.add_keyboard('road_choose', buttons)
     await message.answer('Пожалуйста выберите 🛠 <b>инструмент</b>',
@@ -39,26 +38,36 @@ async def choose_tool(message: aiogram.types.Message, state: FSMContext):
 async def pomodoro(message: aiogram.types.Message,
                    time_focused: int = 15,
                    time_relax: int = 15):
-    await message.answer('Вы включили 🕔 <b>помидор</b>',
-                         reply_markup=ReplyKeyboardRemove())
+    await message.answer(
+        'Вы включили 🕔 <b>помидор</b>',
+        reply_markup=ReplyKeyboardRemove())
 
-    msg = await message.answer('У вас <i>15</i> минут <b>будьте сконцентрированы</b>')
-    await timer(message['from']['id'], time_focused, text='<i>Вжаривай по полной</i>')
+    msg = await message.answer(
+        'У вас <i>15</i> минут <b>будьте сконцентрированы</b>')
+    await timer(
+        message['from']['id'],
+        time_focused,
+        text='<i>Вжаривай по полной</i>')
 
     await msg.edit_text('Теперь у вас есть время на отдых <i>15 минут</i>')
-    await timer(message['from']['id'], time_relax, text='<i>На чиле</i>')
+    await timer(
+        message['from']['id'],
+        time_relax,
+        text='<i>На чиле</i>')
 
-    Session = sessionmaker(bind=engine)
     with Session.begin() as session:
-        user = session.query(PomodoroStats).filter_by(
+        user = session.query(
+            PomodoroStats).filter_by(
             user_id=message.from_user.id).first()
         user.today_cnt += 1
         cnt = user.today_cnt
-    await msg.edit_text('<b>Поздравляю</b> вы получили <b>[<i>%s</i>🍅]</b>' % cnt)
+    await msg.edit_text(
+        '<b>Поздравляю</b> вы получили <b>[<i>%s</i>🍅]</b>' % cnt)
 
     bot.add_keyboard('choose_bool', [['Да', 'Нет']])
-    data['msg_if_restart'] = await message.answer('Хотите начать новый помидор?',
-                                                  reply_markup=bot.keyboards['choose_bool'])
+    data['msg_if_restart'] = await message.answer(
+        'Хотите начать новый помидор?',
+        reply_markup=bot.keyboards['choose_bool'])
     bot.add_state_handler(FSM.choose_bool, choose_bool)
     await FSM.choose_bool.set()
 
@@ -94,7 +103,9 @@ async def timer(chat_id: str, seconds: int,
 
 
 async def habit_tracker(message: aiogram.types.Message):
-    await message.answer('Вы включили трекер привычек', reply_markup=ReplyKeyboardRemove())
+    await message.answer(
+        'Вы включили трекер привычек',
+        reply_markup=ReplyKeyboardRemove())
     await message.answer('<b>Поздравляю</b> вы получили 🦣')
 
 
