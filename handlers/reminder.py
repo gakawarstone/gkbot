f833bfc87c934172892c2fe91da0cf6a79eb29d7
gakawarstone <pinnekerv@gmail.com>
2022-01-24 10:56:41 +0600

    refactor: move Tasks to lib.schedule

--- handlers/reminder.py
+++ handlers/reminder.py
@@ -1,8 +1,13 @@
-from bot_config import bot, schedule
+import datetime
+
 import aiogram
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-import datetime
+from aiogram.types import Message
+
+from bot_config import bot, schedule
+from lib.schedule import Task
+
 data = {}
 
 
@@ -16,38 +21,26 @@ async def add(message: aiogram.types.Message):
 
 async def get_mes(message: aiogram.types.Message, state: FSMContext):
     await state.finish()
-    data['mes'] = message.text
+    data['text'] = message.text
     await message.answer('Отправьте дату и время')
     bot.add_state_handler(FSM.get_time, get_time)
     await FSM.get_time.set()
 
 
-async def get_time(message: aiogram.types.Message, state: FSMContext):
+async def get_time(message: Message, state: FSMContext):
     try:
         await state.finish()
-        mes = data['mes']
-        time = message.text
-        datetime.datetime.strptime(time, '%d.%m.%Y_%H:%M')
-        task = Task(bot.send_message, [
-            message['from']['id'],
-            mes])
-        schedule.add_task_at(task.run, time)
+        text = data['text']
+        time = datetime.datetime.strptime(message.text, '%d.%m.%Y_%H:%M')
+        task = Task.create(
+            bot.send_message,
+            [message.from_user.id, text]
+        ).at(time)
+        schedule.add_task(task)
         await message.answer('Напоминание добавлено')
     except(Exception):
         await message.answer('Формат [30.12.2021_10:28]')
         await FSM.get_time.set()
-
-
-class Task:
-    def __init__(self, func, args=None):
-        self.__func = func
-        self.__args = args
-
-    async def run(self):
-        if self.__args:
-            await self.__func(*self.__args)
-        else:
-            await self.__func()
 
 
 class FSM(StatesGroup):
--- lib/schedule.py
+++ lib/schedule.py
@@ -1,26 +1,47 @@
 import asyncio
 from datetime import datetime, timezone, timedelta
+from typing import Awaitable, Any
+
+
+class Task:
+    def __init__(self, func, args=None) -> None:
+        self.__func = func
+        self.__args = args
+        self.time = None
+
+    async def run(self) -> None:
+        if self.__args:
+            await self.__func(*self.__args)
+        else:
+            await self.__func()
+
+    @staticmethod
+    def create(func: Awaitable, args: list[Any] = None) -> object:
+        task = Task(func, args)
+        return task
+
+    def at(self, time: datetime) -> object:
+        self.time = time
+        return self
 
 
 class Schedule:
     def __init__(self):
         self.tasks = []
 
-    def add_task_at(self, func, time):
-        task = [func, time]
+    def add_task(self, task: Task):
         self.tasks.append(task)
-        print('task added at', task[1])
+        print('task added at', task.time)
 
     async def __dispatcher(self, delay=5, tz=6.0):
         while True:
             timezone_offset = tz
             tzinfo = timezone(timedelta(hours=timezone_offset))
             for task in self.tasks:
-                now = datetime.now(tzinfo).strftime('%d.%m.%Y_%H:%M')
-                time = task[1]
+                now = datetime.strftime(datetime.now(tzinfo), '%d.%m.%Y_%H:%M')
+                time = datetime.strftime(task.time, '%d.%m.%Y_%H:%M')
                 if time == now:
-                    func = task[0]
-                    await func()
+                    await task.run()
                     self.tasks.remove(task)
             await asyncio.sleep(delay)
 
