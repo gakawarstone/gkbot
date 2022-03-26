7ec93edf50ffcb46b699d6d7698ad43e94973aa9
Gakawarstone <pinnekerv@gmail.com>
2022-03-26 17:10:35 +0600

    feat: refactor

--- bot_config.py
+++ bot_config.py
@@ -5,34 +5,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from lib.bot import Bot
-from lib.DBapi import Local, PostgreSQL
 from lib.schedule import Schedule
 
 load_dotenv()
 
 # VARS
-IN_HEROKU = os.environ.get('IN_HEROKU')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-NOTION_TOKEN = os.environ.get('NOTION_TOKEN')
 NOTION_API_TOKEN = os.environ.get('NOTION_API_TOKEN')
-
-if IN_HEROKU:
-    # Heroku PostgreSQL server
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    db = PostgreSQL(DATABASE_URL)
-    engine = create_engine(
-        'postgresql:' + ':'.join(DATABASE_URL.split(':')[1:]))
-else:
-    LOCAL_DB_USER_PSWD = os.environ.get('LOCAL_DB_USER_PSWD').split()
-    db_user = LOCAL_DB_USER_PSWD[0]
-    db_password = LOCAL_DB_USER_PSWD[1]
-    db = Local('bot', db_user, db_password)
-    engine = create_engine(
-        f'postgresql://{db_user}:{db_password}@localhost:5432/bot')
+DB_URL = os.environ.get('DB_URL')
 
 # main objects
 bot = Bot(BOT_TOKEN)
 schedule = Schedule()
+engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
 
 # config
