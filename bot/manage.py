251c43a2842aef47b5ccd88c482129861e86f413
Gakawarstone <pinnekerv@gmail.com>
2022-05-08 10:34:24 +0600

    feat: add bot commands utilit

--- bot/manage.py
+++ bot/manage.py
@@ -1,19 +1,35 @@
+from email.policy import default
 from bot_config import admins, bot, schedule
 from lib.shiki import UserUpdatesDispatcher
 import handlers
 from handlers.help import list_of_commands
 from utils.notify import notify_admins
+from utils.commands import DefaultCommands
+
+default_commands = {
+    'list': 'list of possible bot commands',
+    'road': 'road to the dream'
+}
+
+tasks_on_startup = [
+    DefaultCommands.set(default_commands).on_startup,
+    schedule.on_startup,
+    UserUpdatesDispatcher().on_startup,
+]
 
 
 def start():
     bot.admins = admins
-    bot.add_on_startup(schedule.on_startup)
-    bot.add_on_startup(UserUpdatesDispatcher().on_startup)
+
+    for task in tasks_on_startup:
+        bot.add_on_startup(task)
+
     bot.add_command_handler('list', list_of_commands)
     for cmd in handlers.users:
         bot.add_command_handler(cmd, handlers.users[cmd])
     for cmd in handlers.admins:
         bot.add_command_handler(cmd, handlers.admins[cmd], admin_only=True)
+
     notify_admins('bot started')
     bot.start()
     notify_admins('bot stopped')
---
+++ bot/utils/commands.py
@@ -0,0 +1,21 @@
+import logging
+from aiogram import Dispatcher
+from aiogram import types
+
+logger = logging.getLogger(__name__)
+
+
+class DefaultCommands:
+    commands = []
+
+    @classmethod
+    def set(cls, commands: dict[str, str]):
+        for cmd in commands:
+            c = types.BotCommand(cmd, commands[cmd])
+            cls.commands.append(c)
+        return cls
+
+    @classmethod
+    async def on_startup(cls, dp: Dispatcher):
+        logger.info('Setting bot commands\n' + str(cls.commands))
+        await dp.bot.set_my_commands(cls.commands)
