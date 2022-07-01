f362cf7229a9bc3196df00c10f58343deaa10f50
Gakawarstone <pinnekerv@gmail.com>
2022-07-01 09:18:13 +0600

    feat: add placeholder

--- bot/handlers/hello.py
+++ bot/handlers/hello.py
@@ -2,11 +2,13 @@ import aiogram
 
 from bot_config import bot
 
+# [ ] delete(move to menu)
+
 
 async def start(message: aiogram.types.message):
     commands = [['/add_row', '/trash', '/bomber'],
                 ['/get_log', '/get_trash']]
-    bot.add_keyboard('all_commands', commands)
+    bot.add_keyboard('all_commands', commands, placeholder='Выберите команду')
     await message.answer('Приветствую тебя!')
     await message.answer('Чем займемся?',
                          reply_markup=bot.keyboards['all_commands'])
