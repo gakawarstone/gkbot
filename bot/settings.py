f40d01c57ac7cbafa6455b731ae7f726d4abef03
Gakawarstone <pinnekerv@gmail.com>
2022-07-03 08:56:59 +0600

    style: settings

--- bot/settings.py
+++ bot/settings.py
@@ -1,4 +1,3 @@
-# [ ] rename to settings.py
 import os
 import logging
 import logging.config
@@ -12,23 +11,23 @@ from lib.schedule import Schedule
 from services.shiki import UserUpdatesDispatcher
 from utils.commands import DefaultCommands
 
+
 # Logging config
 logging.basicConfig(level=logging.WARNING,
                     format='%(name)s::%(levelname)s::%(message)s')
 logger = logging.getLogger(__name__)
 
+
 # Environmental variables
 load_dotenv()
-
-# VARS
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 NOTION_API_TOKEN = os.getenv('NOTION_API_TOKEN')
 DB_URL = os.getenv('DB_URL')
 
-# Log info vars
 logger.info('BOT_TOKEN = ' + BOT_TOKEN)
 logger.info('NOTION_API_TOKEN = ' + NOTION_API_TOKEN)
 logger.info('DB_URL = ' + DB_URL)
+
 
 # Main objects
 bot = Bot(BOT_TOKEN)
@@ -36,9 +35,11 @@ engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
 schedule = Schedule()
 
+
 ADMINS = [
     897651738
 ]
+
 
 DEFAULT_COMMANDS = {
     'list': 'list of possible bot commands',
@@ -47,6 +48,7 @@ DEFAULT_COMMANDS = {
     'add_remind': 'add remind',
     'start_timer': 'start timer',
 }
+
 
 TASKS_ON_STARTUP = [
     DefaultCommands.set(DEFAULT_COMMANDS).on_startup,
