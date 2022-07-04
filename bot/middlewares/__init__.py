bd0b2b194b4df3ccb954036aed3dd6e05163dec6
Gakawarstone <pinnekerv@gmail.com>
2022-07-04 11:16:08 +0600

    fix: user data middleware

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,6 +1,8 @@
 from settings import bot
 from .register_user import RegisterUserMiddleware
+from .user_data import UserDataMiddleware
 
 
 def setup():
     bot.dp.middleware.setup(RegisterUserMiddleware())
+    bot.dp.middleware.setup(UserDataMiddleware())
