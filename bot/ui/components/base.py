096d10449bda1fd37ee7d1eb95d7ec9d45163653
Gakawarstone <pinnekerv@gmail.com>
2022-07-14 19:39:05 +0200

    refactor: add base component

--- bot/ui/components/base.py
+++ bot/ui/components/base.py
@@ -1,1 +1,24 @@
-# [ ]
+from abc import ABC, abstractmethod
+from settings import mng
+
+
+class BaseComponent(ABC):
+    def __init__(self, user_id: int) -> None:
+        self.user_id = user_id
+        self.__message = None
+        self._state = None
+        self.__rendered_text = None
+
+    @property
+    @abstractmethod
+    async def content(self):
+        pass
+
+    async def _render(self):
+        if not self.__message:
+            self.__message = await mng.send_message(
+                self.user_id, await self.content)
+        elif await self.content != self.__rendered_text:
+            await self.__message.edit_text(await self.content)
+
+        self.__rendered_text = self.content
--- bot/ui/components/remind_creator.py
+++ bot/ui/components/remind_creator.py
@@ -1,54 +1,52 @@
-from settings import mng
-# [ ] add base component
+from .base import BaseComponent
 
 
-class RemindCreator:
-    __state = 'remind_text'
-    __rendered_text = None
-    __message = None
-    __text = None
-    __date = None
-    __time = None
-    __status_message = None
-
+class RemindCreator(BaseComponent):
     def __init__(self, user_id: int) -> None:
-        self.user_id = user_id
+        super().__init__(user_id)
+        self.__state = 'remind_text'
+        self.state = 'remind_text'
+        self.text = None
+        self.date = None
+        self.__time = None
+        self.__status_message = None
 
     async def init(self):
-        self.__state = 'remind_text'
-        await self.__render()
+        await self._render()
 
     async def set_remind_text(self, text: str) -> None:
         # [ ] only after init
         self.__text = text
         self.__state = 'remind_date'
-        await self.__render()
+        await self._render()
 
     async def set_remind_date(self, date: str) -> None:
         # [ ] only after text
         self.__date = date
         self.__state = 'remind_time'
-        await self.__render()
+        await self._render()
 
     async def set_remind_time(self, time: str) -> None:
         # [ ] only after date
         self.__time = time
         self.__state = 'finished'
-        await self.__render()
+        await self._render()
         self.clear_status_message()
 
     async def set_status_finished(self) -> None:
         await self.set_status_message('<u>Создано успешно</u> ✅')
+        self.__message = None  # FIXME
 
     async def set_status_message(self, text: str) -> None:
         self.__status_message = text
-        await self.__render()
+        await self._render()
         self.clear_status_message()
 
     def clear_status_message(self) -> None:
         self.__status_message = None
 
-    async def __render(self):  # [ ] as decorator
+    @property
+    async def content(self):
         text = '<u>Создатель напоминаний ⌚️</u>\n'
 
         if self.__state == 'remind_text':
@@ -69,9 +67,4 @@ class RemindCreator:
         if self.__status_message:
             text += self.__status_message
 
-        if not self.__message:  # FIXME
-            self.__message = await mng.send_message(self.user_id, text)
-        elif text != self.__rendered_text:
-            await self.__message.edit_text(text)
-
-        self.__rendered_text = text
+        return text
