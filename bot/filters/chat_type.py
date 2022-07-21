092bc7b305abdd6b74d3cea630a833397df128b6
Gakawarstone <pinnekerv@gmail.com>
2022-07-21 10:04:17 +0200

    feat: add chat type filter

---
+++ bot/filters/chat_type.py
@@ -0,0 +1,14 @@
+from aiogram.dispatcher.filters import BaseFilter
+from aiogram.types import Message
+
+from lib.types import ChatType
+
+
+class ChatTypeFilter(BaseFilter):
+    chat_type: ChatType | list[ChatType]
+
+    async def __call__(self, message: Message) -> bool:
+        if type(self.chat_type) == ChatType:
+            return ChatType(message.chat.type) == self.chat_type
+        else:
+            return ChatType(message.chat.type) in self.chat_type
