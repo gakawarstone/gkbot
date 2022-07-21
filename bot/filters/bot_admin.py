fffe462653650f1730f8fe2e74229ed41bf2dee0
Gakawarstone <pinnekerv@gmail.com>
2022-07-21 19:51:46 +0200

    feat: add bot admin filter

---
+++ bot/filters/bot_admin.py
@@ -0,0 +1,11 @@
+from aiogram.dispatcher.filters import BaseFilter
+from aiogram.types import Message
+
+from settings import ADMINS
+
+
+class BotAdmin(BaseFilter):
+    admins = ADMINS
+
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in self.admins
