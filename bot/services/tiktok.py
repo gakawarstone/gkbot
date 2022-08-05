003a05d8c7c8a2fe62c12cfd2fc74a96ce62b3ad
Gakawarstone <pinnekerv@gmail.com>
2022-08-05 14:34:26 +0200

    fix: tt downloader

--- bot/services/tiktok.py
+++ bot/services/tiktok.py
@@ -1,24 +1,38 @@
-from tiktok_downloader import snaptik
-from tiktok_downloader.utils import info_videotiktok
-from tiktok_downloader.Except import InvalidUrl
+from dataclasses import dataclass
+import requests
 
 
-class TikTokInvalidUrl(InvalidUrl):
+@dataclass
+class InfoVideoTikTok:
+    video_url: str
+    music_url: str
+
+
+def info_video_tiktok_serializer(data: dict) -> InfoVideoTikTok:
+    return InfoVideoTikTok(
+        video_url=data['nwm_video_url'],
+        music_url=data['video_music_url']
+    )
+
+
+class TikTokInvalidUrl(Exception):
     def __init__(self, url: str) -> None:
         self.url = url
 
     def __str__(self) -> str:
-        return f'Invalid url {self.url}'
+        return f'Invalid TikTok video url {self.url}'
 
 
 class TikTokDownloader:
     @classmethod
-    def __get_video(cls, url: str) -> info_videotiktok:
-        return snaptik(url).get_media()[0]
+    def __get_video_info(cls, url: str) -> InfoVideoTikTok:
+        try:
+            return info_video_tiktok_serializer(
+                requests.get(f'https://api.douyin.wtf/api?url={url}').json()
+            )
+        except KeyError:
+            raise TikTokInvalidUrl(url)
 
     @classmethod
     def get_video_url(cls, url: str) -> str:
-        try:
-            return cls.__get_video(url).json
-        except InvalidUrl:
-            raise TikTokInvalidUrl(url)
+        return cls.__get_video_info(url).video_url
