feb17ca290c7082b1dbadab40d0f6e77f0af8a03
Gakawarstone <pinnekerv@gmail.com>
2022-10-04 10:51:10 +0200

    feat: update widget ui

--- bot/ui/components/books.py
+++ bot/ui/components/books.py
@@ -4,8 +4,8 @@ from models.books import Book
 class BookComponent:
     @classmethod
     def render(cls, book: Book) -> str:
-        text = f'<b>Название ✍️:</b>                  {book.name}\n'
-        text += f'<b>Автор 👨‍🦳:</b>                         {book.author}\n'
-        text += '<b>Прогресс(главы) 📈:</b>     '
+        text = f'✍ Название :: {book.name}\n\n'
+        text += f'👨‍🦳 Автор :: {book.author}\n\n'
+        text += '📈 Прогресс(главы) :: '
         text += f'<b>{book.current_chapter}</b> из {book.chapters_cnt}'
         return text
