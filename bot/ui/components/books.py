aa3fb1fd966914daf58e823008190e34b9270b2d
Gakawarstone <pinnekerv@gmail.com>
2022-10-08 15:59:28 +0200

    feat: update book widget

--- bot/ui/components/books.py
+++ bot/ui/components/books.py
@@ -5,7 +5,7 @@ class BookComponent:
     @classmethod
     def render(cls, book: Book) -> str:
         text = f'✍ Название :: {book.name}\n\n'
-        text += f'👨‍🦳 Автор :: {book.author}\n\n'
+        text += f'🗿 Автор :: {book.author}\n\n'
         text += '📈 Прогресс(главы) :: '
         text += f'<b>{book.current_chapter}</b> из {book.chapters_cnt}'
         return text
