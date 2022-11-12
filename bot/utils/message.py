1dced53571001a6216f5e41aa959a1db8d02eb1b
Gakawarstone <pinnekerv@gmail.com>
2022-11-12 12:12:09 +0100

    chore: add label

--- bot/utils/message.py
+++ bot/utils/message.py
@@ -1,6 +1,9 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+# FIXME deprecated use delete queue middleware with one time message
+# FIXME handler extension
+
 
 async def delete_previous_message(message: Message, state: FSMContext):
     await state.bot.delete_message(
