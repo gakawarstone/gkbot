08584829709cdd0ee9bb1b56860abc530fe500e6
Gakawarstone <pinnekerv@gmail.com>
2022-11-14 09:41:35 +0100

    refactor: add base context manager

---
+++ bot/contrib/handlers/message/context_manager.py
@@ -0,0 +1,58 @@
+from typing import Any, Optional
+from dataclasses import dataclass
+
+from .base import BaseHandler as _BaseHandler
+
+
+@dataclass
+class Property:
+    name: str
+    type: type
+
+
+class BaseContextManager(_BaseHandler):
+    props: Any
+    _context_type = type
+
+    @property
+    def ctx(self) -> _context_type:
+        props_name_list: list[str] = [
+            prop_name
+            for prop_name in dir(self.props)
+            if not prop_name.startswith('__')
+        ]
+
+        context_args = {}
+        for prop_name in props_name_list:
+            context_args[prop_name] = self._try_get_prop(
+                getattr(self.props, prop_name))
+
+        return self._context_type(**context_args)
+
+    def set(self, prop: Property, value: Any) -> None:
+        if type(value) is not prop.type:
+            raise ValueError
+
+        self.user_data[prop.name] = value
+
+    def clean_context(self, exclude: list[Property] = []):
+        for prop_name in dir(self.props):
+            if prop_name.startswith('__'):
+                continue
+
+            if prop_name in [p.name for p in exclude]:
+                continue
+
+            prop = getattr(self.props, prop_name)
+            self._reset_prop(prop)
+
+    def _reset_prop(self, prop: Property) -> None:
+        self.user_data[prop.name] = None
+
+    def _try_get_prop(self, prop: Property) -> Optional[Any]:
+        value = self._try_get_from_user_data(prop.name)
+
+        if value is not None and type(value) is not prop.type:
+            raise ValueError
+
+        return value
--- bot/handlers/user/reminder/_context.py
+++ bot/handlers/user/reminder/_context.py
@@ -1,24 +1,18 @@
 from datetime import date, time, timedelta
 from dataclasses import dataclass
-from typing import Optional, Any
+from typing import Optional
 
 from aiogram.types import Message
 
-from contrib.handlers.message.base import BaseHandler as _BaseHandler
-
-
-@dataclass
-class _Property:
-    name: str
-    type: type
+from contrib.handlers.message.context_manager import BaseContextManager, Property
 
 
 class _Properties:
-    text = _Property('text', str)
-    tz = _Property('tz', timedelta)
-    date = _Property('date', date)
-    time = _Property('time', time)
-    message = _Property('mes', Message)
+    text = Property('text', str)
+    tz = Property('tz', timedelta)
+    date = Property('date', date)
+    time = Property('time', time)
+    message = Property('mes', Message)
 
 
 @dataclass
@@ -30,43 +24,6 @@ class _ReminderContext:
     message: Optional[_Properties.message.type]
 
 
-class ReminderContextManager(_BaseHandler):
+class ReminderContextManager(BaseContextManager):
     props = _Properties
-
-    @property
-    def ctx(self) -> _ReminderContext:
-        return _ReminderContext(
-            text=self._try_get_prop(self.props.text),
-            tz=self._try_get_prop(self.props.tz),
-            date=self._try_get_prop(self.props.date),
-            time=self._try_get_prop(self.props.time),
-            message=self._try_get_prop(self.props.message)
-        )
-
-    def set(self, prop: _Property, value: Any) -> None:
-        if type(value) is not prop.type:
-            raise ValueError
-
-        self.user_data[prop.name] = value
-
-    def clean_context(self, exclude: list[_Property] = []):
-        for prop_name in dir(self.props):
-            if prop_name.startswith('__'):
-                continue
-
-            if prop_name in [p.name for p in exclude]:
-                continue
-
-            prop = getattr(self.props, prop_name)
-            self._reset_prop(prop)
-
-    def _reset_prop(self, prop: _Property) -> None:
-        self.user_data[prop.name] = None
-
-    def _try_get_prop(self, prop: _Property) -> Optional[Any]:
-        value = self._try_get_from_user_data(prop.name)
-
-        if value is not None and type(value) is not prop.type:
-            raise ValueError
-
-        return value
+    _context_type = _ReminderContext
