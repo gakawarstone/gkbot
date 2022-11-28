2741db66ae16ad6ac2ba1e0527ec5f287a9b48da
Gakawarstone <pinnekerv@gmail.com>
2022-11-28 09:07:47 +0100

    fix: book add UI

--- bot/handlers/user/books/messages/add/get_author.py
+++ bot/handlers/user/books/messages/add/get_author.py
@@ -8,4 +8,4 @@ class GetAuthorHandler(BaseHandler):
     async def handle(self) -> Any:
         await self.state.set_state(FSM.get_chapters_cnt)
         self.set(self.props.author, self.event.text)
-        await self.event.answer('Максимальный прогресс?')
+        await self.event.answer('Количество страниц?')
