af6a1a94e9b4ccbcf779fe1117bfaf10183b96b8
Gakawarstone <pinnekerv@gmail.com>
2022-12-17 19:23:47 +0100

    fix: file_path if local api server

--- bot/contrib/handlers/message/file_extension.py
+++ bot/contrib/handlers/message/file_extension.py
@@ -15,13 +15,13 @@ class FileHandlerExtension(BaseHandler):
         if not self.event.document:
             raise NoFileException
 
+        file = await self.state.bot.get_file(self.event.document.file_id)
+        file_path = file.file_path
+
         if API_SERVER_URL == 'https://api.telegram.org':
-            file = await self.state.bot.get_file(self.event.document.file_id)
-            file_path = file.file_path
             file_io = BytesIO()
             await self.state.bot.download_file(file_path, file_io)
         else:
-            file_path = self.event.document.file_id
             file_io = BytesIO(open(file_path, 'rb').read())
 
         return file_io
