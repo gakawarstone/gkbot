f76efc2c2ad4dd2d36a302bde08c23e5580a3edc
Gakawarstone <pinnekerv@gmail.com>
2023-03-05 16:04:30 +0100

    fix: tt download long link

--- bot/services/tiktok/downloaders/proxytok.py
+++ bot/services/tiktok/downloaders/proxytok.py
@@ -12,23 +12,24 @@ class ProxyTok(BaseDownloader):
     async def get_video_info(self, url: str) -> InfoVideoTikTok:
         try:
             return InfoVideoTikTok(
-                video_url=await self._get_video_url(url),
+                video_url=await self._get_video_file_url(url),
                 music_url='deprecated'
             )
         except (KeyError, IndexError, ValueError, ClientConnectionError):
             raise SourceDownloadFailed(self)
 
-    async def _get_video_url(self, url: str) -> str:
+    async def _get_video_file_url(self, url: str) -> str:
+        url_in_proxytok = self._get_url_in_proxytok(url)
+        video_file_url = await self._find_video_file_url(url_in_proxytok)
+
+        if not self._is_link_relative(video_file_url):
+            return video_file_url
+        return self.__instance_url + video_file_url
+
+    def _get_url_in_proxytok(self, url: str) -> str:
         if not self._is_short_link(url):
-            video_url = self.__instance_url + '/download?url=' + url
-        else:
-            video_code = self._get_video_code(url)
-            video_url = await self._get_video_url_from_code(video_code)
-
-        if not self._is_link_relative(video_url):
-            return video_url
-
-        return self.__instance_url + video_url
+            return self.__instance_url + '/@' + url.split('@')[-1]
+        return self.__instance_url + '/t/' + self._get_video_code(url)
 
     def _is_short_link(self, url: str) -> bool:
         return not '@' in url
@@ -36,8 +37,7 @@ class ProxyTok(BaseDownloader):
     def _get_video_code(self, url: str) -> str:
         return url.split('/')[-2]
 
-    async def _get_video_url_from_code(self, code: str) -> str:
-        url_in_proxytok = self.__instance_url + '/t/' + code
+    async def _find_video_file_url(self, url_in_proxytok: str) -> str:
         soup = await self._get_soup(url_in_proxytok)
         href = soup.find_all('a')[-1]['href']
 
@@ -47,7 +47,7 @@ class ProxyTok(BaseDownloader):
         return href
 
     def _is_link_valid(self, url: str) -> bool:
-        return url.startswith(self.__instance_url)
+        return url.startswith((self.__instance_url, '/download'))
 
     def _is_link_relative(self, url: str) -> bool:
         return not url.startswith('http')
