276f94deda6c35ff7c15b7ff23cd300f01558048
Gakawarstone <pinnekerv@gmail.com>
2023-04-07 09:46:18 +0200

    fix: add road_greet image to static

--- bot/handlers/user/road/start.py
+++ bot/handlers/user/road/start.py
@@ -3,23 +3,16 @@ from aiogram.filters import StateFilter, Command, or_f
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from settings import logger
+from services.static import Images
 from ui.keyboards.road import RoadMarkup
 from .._commands import USER_COMMANDS
 from .states import FSM
 
 
-# [ ] move to menu?
-
-
 async def start(message: Message, state: FSMContext):
     await state.set_state(FSM.menu)
-    logger.debug('Road handler started')
-    photo_id = 'AgACAgIAAxkDAALtVWHn3ZZmzpMfA3SI7usT1avw9xrWAALRtjEbe9FASzJZxPBxsVhdAQADAgADeQADIwQ'
-    logger.warning(
-        'Make sure that bot can use this photo_id: ' + photo_id)  # BUG
     await message.answer_photo(
-        photo_id,
+        Images.road_greet,
         caption='Привет <i>%s</i> ты включил модуль 🚀<b>РОД ЗЕ ДРИМ</b>🚀' %
                 message.from_user.first_name)
     await message.answer('Пожалуйста выберите 🛠 <b>инструмент</b>',
--- bot/services/static.py
+++ bot/services/static.py
@@ -3,3 +3,4 @@ from aiogram.types.input_file import FSInputFile
 
 class Images:
     book_shelf = FSInputFile('./static/book_shelf.png')
+    road_greet = FSInputFile('./static/road_greet.png')
