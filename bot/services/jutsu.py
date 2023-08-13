d414bb4791905bc7f0c8998ce0fe8004d6edbde4
Gakawarstone <pinnekerv@gmail.com>
2023-08-13 21:42:34 +0200

    refactor: jutsu downloader

--- bot/services/jutsu.py
+++ bot/services/jutsu.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from typing import TypeAlias
 from aiogram.types import BufferedInputFile
-from bs4 import BeautifulSoup, ResultSet, SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer
 
-from .base_downloader import BaseDownloader
+from services.http import HttpService, HttpRequestError
 
 _Link: TypeAlias = str
 _Pixel: TypeAlias = int
@@ -19,28 +19,23 @@ class InvalidUrl(Exception):
     "Jut.su invalid url"
 
 
-class JutSuDownloader(BaseDownloader):
+class JutSuDownloader:
     @classmethod
     async def download_video(cls, url: str) -> BufferedInputFile:
         try:
-            low_resolution_source = cls.__get_sources(url)[-1]
-            file = await cls._download_file_from_url(
-                low_resolution_source.link)
-        except IndexError:
+            video_url = (await cls.__get_sources(url))[-1].link
+            file = await HttpService.get(video_url)
+        except (IndexError, HttpRequestError):
             raise InvalidUrl
         return BufferedInputFile(file, 'video.mp4')
 
     @classmethod
-    def __get_sources(cls, url: str) -> list[_Source]:
-        soup = cls._get_soup(url)
+    async def __get_sources(cls, url: str) -> list[_Source]:
+        soup = await cls.__get_soup(url)
         return [
             cls.__format_source(elem)
-            for elem in cls.__find_raw_sources(soup)
+            for elem in soup.find_all('video')[0].find_all('source')
         ]
-
-    @staticmethod
-    def __find_raw_sources(soup: BeautifulSoup) -> ResultSet:
-        return soup.find_all('video')[0].find_all('source')
 
     @staticmethod
     def __format_source(element: SoupStrainer) -> _Source:
@@ -48,3 +43,7 @@ class JutSuDownloader(BaseDownloader):
             resolution=int(element['label'][:-1]),
             link=element['src']
         )
+
+    @staticmethod
+    async def __get_soup(url: str) -> BeautifulSoup:
+        return BeautifulSoup(await HttpService.get(url), 'html.parser')
