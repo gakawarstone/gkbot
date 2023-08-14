d000fec455335b2dae94bc0b94891bc034aaef19
Gakawarstone <pinnekerv@gmail.com>
2023-08-14 18:37:22 +0200

    fix: tiktok video link if no text

--- bot/filters/tiktok.py
+++ bot/filters/tiktok.py
@@ -9,5 +9,7 @@ class TikTokVideoLink(BaseFilter):
 
     async def __call__(self, telegram_object: Message | InlineQuery):
         if isinstance(telegram_object, Message):
+            if not telegram_object.text:
+                return False
             return self.pattern.match(telegram_object.text)
         return self.pattern.match(telegram_object.query)
