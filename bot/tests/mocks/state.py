8664f3efa934598f581540a6814abd70ef4096b0
Gakawarstone <pinnekerv@gmail.com>
2023-08-28 20:41:58 +0200

    fix: remove bot from context

--- bot/tests/mocks/state.py
+++ bot/tests/mocks/state.py
@@ -1,12 +1,9 @@
-from unittest.mock import AsyncMock
-
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.storage.base import StorageKey
 
 
 fake_state = FSMContext(
-    bot=AsyncMock(),
     storage=MemoryStorage(),
     key=StorageKey(
         bot_id=1,
