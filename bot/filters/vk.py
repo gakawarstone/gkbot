342f8a8f0e54fe23e9fa9d845bd730edfb0a787a
Gakawarstone <pinnekerv@gmail.com>
2023-08-30 18:20:57 +0200

    fix: filters

--- bot/filters/vk.py
+++ bot/filters/vk.py
@@ -8,4 +8,8 @@ class VkVideoLink(BaseFilter):
     __pattern = r'https:\/\/vk\.com\/video(-?\d+)_(\d+)'
 
     async def __call__(self, telegram_object: Message):
+        if not isinstance(telegram_object, Message):
+            return False
+        if not telegram_object.text:
+            return False
         return re.search(self.__pattern, telegram_object.text)
--- bot/filters/youtube.py
+++ bot/filters/youtube.py
@@ -9,4 +9,8 @@ class YouTubeVideoLink(BaseFilter):
                  r'v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?')
 
     async def __call__(self, telegram_object: Message):
+        if not isinstance(telegram_object, Message):
+            return False
+        if not telegram_object.text:
+            return False
         return re.search(self.__pattern, telegram_object.text)
