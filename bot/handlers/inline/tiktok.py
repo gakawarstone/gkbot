15d3e753bc163dc7da3527b1a4767abbd4402ffd
Gakawarstone <pinnekerv@gmail.com>
2023-08-30 13:43:21 +0200

    refactor: tiktok inline

--- bot/handlers/inline/tiktok.py
+++ bot/handlers/inline/tiktok.py
@@ -1,19 +1,25 @@
 from aiogram import Router
-from aiogram.types import InlineQuery
+from aiogram.types import InlineQuery, InlineQueryResultVideo
 
 from services.tiktok import TikTokService
-from utils.serializers import url_to_video_query_result
 from filters.tiktok import TikTokVideoLink
 
 
 async def send_tiktok(query: InlineQuery):
+    video_url = await TikTokService.get_video_url(query.query)
     return await query.answer(
         results=[
-            url_to_video_query_result(
-                video_url=await TikTokService.get_video_url(query.query),
-                title='TikTok',
-                description='tap to send'
-            )
+            InlineQueryResultVideo(
+                id=query.query,
+                type='video',
+                title='Tiktok video',
+                description='tap to send',
+                video_url=video_url,
+                mime_type='video/mp4',
+                thumbnail_url=video_url,
+                thumb_url=video_url,
+                caption=query.query,
+            ),
         ],
         cache_time=60,
         is_personal=True
