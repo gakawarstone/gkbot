ff93abb68d50ae28c231b069b3e9612d9adb0ccf
Gakawarstone <pinnekerv@gmail.com>
2023-10-11 10:40:44 +0200

    feat: new component style

--- bot/ui/components/base.py
+++ bot/ui/components/base.py
@@ -5,12 +5,12 @@ class BaseComponent:
     def _highlight_if(self, statement: bool, text: str) -> str:
         if not statement:
             return text
-        return '<b>> ' + text + '</b>'
+        return "<b> " + text + "</b>👈"
 
     def _is_property_not_exist(self, prop: Any) -> bool:
         return prop is None
 
-    def _render_if_exist(self, prop: Any, text_else: str = '') -> str:
+    def _render_if_exist(self, prop: Any, text_else: str = "") -> str:
         if prop is not None:
-            return prop + ' '
+            return prop + " "
         return text_else
