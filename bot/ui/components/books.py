68bc4a641207125781ca5f791942497927e77054
Gakawarstone <pinnekerv@gmail.com>
2023-10-11 11:06:02 +0200

    feat: new style book component

--- bot/ui/components/books.py
+++ bot/ui/components/books.py
@@ -4,8 +4,8 @@ from models.books import Book
 class BookComponent:
     @classmethod
     def render(cls, book: Book) -> str:
-        text = f"✍ Название :: {book.name}\n\n"
-        text += f"🗿 Автор :: {book.author}\n\n"
-        text += "📈 Прогресс :: "
-        text += f"<b>{book.current_chapter}</b> из {book.chapters_cnt}"
+        text = f"✍Название:: {book.name}\n\n"
+        text += f"🗿Автор:: {book.author}\n\n"
+        text += "📈Прогресс:: "
+        text += f"<b>{book.current_chapter}</b>/{book.chapters_cnt}"
         return text
