2827ba389642f69428ba236fe92332a32d9fe11b
Gakawarstone <pinnekerv@gmail.com>
2023-10-11 11:35:37 +0200

    feat: delete menu command in show books

--- bot/handlers/user/books/messages/show.py
+++ bot/handlers/user/books/messages/show.py
@@ -10,6 +10,7 @@ from ..states import FSM
 
 async def show_user_books(message: Message, state: FSMContext):
     await state.set_state(FSM.check_menu_command)
+    await message.delete()
     await message.answer(
         "Ваши книги: ",
         reply_markup=EventsMarkup.get_books_dialog(
