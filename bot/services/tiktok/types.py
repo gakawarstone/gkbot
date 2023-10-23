987a32bdad8a25cba6f0f779d34be415da238403
Gakawarstone <pinnekerv@gmail.com>
2023-10-23 20:45:52 +0200

    chore: typing

--- bot/services/tiktok/types.py
+++ bot/services/tiktok/types.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class InfoVideoTikTok:
-    video_url: str
-    music_url: str
-    images_urls: list[str]
+    video_url: Optional[str]
+    music_url: Optional[str]
+    images_urls: Optional[list[str]]
