bf1b1c4bd19f8907a94398f2c039908b9b2c53b7
Gakawarstone <pinnekerv@gmail.com>
2023-10-23 20:48:08 +0200

    feat: show current date with date picker keyboard

--- bot/ui/keyboards/reminder.py
+++ bot/ui/keyboards/reminder.py
@@ -1,19 +1,19 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from lib.keyboard_builder import KeyboardBuilder
 
 
 @dataclass
 class _Buttons:
-    today = 'Сегодня'
-    tomorrow = 'Завтра'
+    today = "Сегодня"
+    tomorrow = "Завтра"
 
 
 class RemindMarkup:
     buttons = _Buttons
 
     date = KeyboardBuilder.add_keyboard(
-        buttons=[
-            [buttons.today, buttons.tomorrow]
-        ]
+        buttons=[[buttons.today, buttons.tomorrow]],
+        placeholder="Сегодня: " + datetime.now().date().strftime("%d.%m.%Y"),
     )
