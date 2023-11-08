df0005e1e1ef5d98c7774a82a51f502207a6cb12
Gakawarstone <pinnekerv@gmail.com>
2023-11-08 11:01:53 +0100

    feat: add inline chance handler

--- bot/handlers/inline/__init__.py
+++ bot/handlers/inline/__init__.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 
-from . import tiktok, youtube
+from . import tiktok, youtube, chance
 
 
 def setup(r: Router):
     tiktok.setup(r)
     youtube.setup(r)
+    chance.setup(r)
---
+++ bot/handlers/inline/chance.py
@@ -0,0 +1,29 @@
+import random
+
+from aiogram import Router, F
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+
+
+async def send_chance(query: InlineQuery):
+    message = f"Шанс того что {' '.join(query.query.split(' ')[1:])}: "
+    chance = random.randint(0, 100)
+    description = "tap to send your chance"
+    results = [
+        InlineQueryResultArticle(
+            id=query.query,
+            title=message,
+            input_message_content=InputTextMessageContent(
+                message_text=message + str(chance) + "%"
+            ),
+            description=description,
+        )
+    ]
+    return await query.answer(
+        results=results,
+        cache_time=24 * 60 * 60,
+        is_personal=False,
+    )
+
+
+def setup(router: Router):
+    router.inline_query.register(send_chance, F.query.startswith(("ch", "ч")))
