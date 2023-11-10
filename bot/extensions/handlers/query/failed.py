c58bb9d64237ad5fca6ba4083b2145e93ca99093
Gakawarstone <pinnekerv@gmail.com>
2023-11-10 16:16:07 +0100

    refactor: add query failed result decorator

--- bot/extensions/handlers/query/failed.py
+++ bot/extensions/handlers/query/failed.py
@@ -1,11 +1,33 @@
+from typing import Type, Callable
+from functools import wraps
+
 from aiogram.types import InputTextMessageContent, InlineQueryResultArticle, InlineQuery
 
 
-def get_failed_result(query: InlineQuery, message: str, description: str):
+def get_failed_result(query: InlineQuery, message_to_send: str, description: str):
     return InlineQueryResultArticle(
         id=query.query,
-        type="article",
         title="Failed",
-        input_message_content=InputTextMessageContent(message_text=message),
+        input_message_content=InputTextMessageContent(message_text=message_to_send),
         description=description,
     )
+
+
+def async_return_failed_result_if(
+    exceptions_to_handle: list[Type[BaseException]],
+    message_to_send: str,
+    description: str,
+) -> Callable:
+    def decorator(func: Callable) -> Callable[[InlineQuery], InlineQueryResultArticle]:
+        @wraps(func)
+        async def wrapper(
+            query: InlineQuery, *args, **kwargs
+        ) -> InlineQueryResultArticle:
+            try:
+                return await func(query, *args, **kwargs)
+            except tuple(exceptions_to_handle):
+                return [get_failed_result(query, message_to_send, description)]
+
+        return wrapper
+
+    return decorator
--- bot/handlers/inline/tiktok.py
+++ bot/handlers/inline/tiktok.py
@@ -2,37 +2,36 @@ from aiogram import Router
 from aiogram.types import InlineQuery, InlineQueryResultVideo
 
 from services.tiktok import TikTokService, TikTokVideoUrlExtractionFailed
-from extensions.handlers.query.failed import get_failed_result
+from extensions.handlers.query.failed import async_return_failed_result_if
 from filters.tiktok import TikTokVideoLink
 
 
-async def send_tiktok(query: InlineQuery):
-    results = []
-    try:
-        video_url = await TikTokService.get_video_url(query.query)
-        results += [
-            InlineQueryResultVideo(
-                id=query.query,
-                type="video",
-                title="Tiktok video",
-                description="tap to send",
-                video_url=video_url,
-                mime_type="video/mp4",
-                thumbnail_url=video_url,
-                thumb_url=video_url,
-                caption=query.query,
-            ),
-        ]
-    except TikTokVideoUrlExtractionFailed:
-        print("failed")
-        results += [
-            get_failed_result(
-                query=query,
-                message=f"download failed: {query.query} try to send this link direct to bot",
-                description="try to send link direct to bot",
-            ),
-        ]
+@async_return_failed_result_if(
+    exceptions_to_handle=[
+        TikTokVideoUrlExtractionFailed,
+    ],
+    message_to_send="download failed: {query} try to send this link direct to bot",
+    description="try to send link direct to bot",
+)
+async def _get_results(query: InlineQuery) -> list[InlineQueryResultVideo]:
+    video_url = await TikTokService.get_video_url(query.query)
+    return [
+        InlineQueryResultVideo(
+            id=query.query,
+            type="video",
+            title="Tiktok video",
+            description="tap to send",
+            video_url=video_url,
+            mime_type="video/mp4",
+            thumbnail_url=video_url,
+            thumb_url=video_url,
+            caption=query.query,
+        ),
+    ]
 
+
+async def send_tiktok(query: InlineQuery):
+    results = await _get_results(query)
     return await query.answer(
         results=results,
         cache_time=24 * 60 * 60,
