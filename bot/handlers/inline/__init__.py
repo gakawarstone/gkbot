d32368afd1a5583a0c238637576c482f68bce798
Gakawarstone <pinnekerv@gmail.com>
2023-11-25 17:19:36 +0100

    feat: add choice from list inline handler

--- bot/handlers/inline/__init__.py
+++ bot/handlers/inline/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 
-from . import tiktok, youtube, chance
+from . import tiktok, youtube, chance, choose_list
 
 
 def setup(r: Router):
     tiktok.setup(r)
     youtube.setup(r)
     chance.setup(r)
+    choose_list.setup(r)
---
+++ bot/handlers/inline/choose_list.py
@@ -0,0 +1,33 @@
+import random
+import hashlib
+
+from aiogram import Router, F
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+
+
+async def send_choice(query: InlineQuery):
+    message = f"Из {', '.join(query.query.split(' ')[1:])} я выбираю: "
+    choices = query.query.split(" ")[1:]
+    choice = random.choice(choices)
+    description = "tap to send your choice"
+
+    results = [
+        InlineQueryResultArticle(
+            id=hashlib.md5(query.query.encode()).hexdigest(),
+            title=message,
+            input_message_content=InputTextMessageContent(
+                message_text=message + str(choice)
+            ),
+            description=description,
+        )
+    ]
+
+    return await query.answer(
+        results=results,
+        cache_time=10,
+        is_personal=False,
+    )
+
+
+def setup(router: Router):
+    router.inline_query.register(send_choice, F.query.startswith(("lst", "лст")))
