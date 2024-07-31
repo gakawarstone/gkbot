e0b082b6c717a8d4b9a9155e01e4bb7b013228e8
gakawarstone <pinnekerv@gmail.com>
2024-07-31 22:13:21 +0200

    fix: hotfix for yt shorts

--- bot/services/ytdlp.py
+++ bot/services/ytdlp.py
@@ -1,4 +1,6 @@
+import re
 from dataclasses import dataclass
+from enum import Enum
 
 import yt_dlp
 from aiogram.types import BufferedInputFile, FSInputFile
@@ -14,27 +16,39 @@ class AudioFileInfo:
     title: str
 
 
-ydl_opts = {
-    "format": "ba",
-    "external_downloader": "aria2c",
-}
-
-vkdl_opts = {
-    "format": "url240",
-    "external_downloader": "aria2c",
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "m4a",
-        }
-    ],
-}
+class DownloadOptions(Enum):
+    pass
 
 
-video_opts = {
-    "format": "worst[ext=mp4]",
-    "external_downloader": "aria2c",
-}
+# FIXME: move to separate file
+class AudioDownloadOptions(DownloadOptions):
+    youtube = {
+        "format": "ba",
+        "external_downloader": "aria2c",
+    }
+
+    vk = {
+        "format": "url240",
+        "external_downloader": "aria2c",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "m4a",
+            }
+        ],
+    }
+
+
+class VideoDownloadOptions(DownloadOptions):
+    youtube_shorts = {
+        "format": "bv+ba",
+        "external_downloader": "aria2c",
+    }
+
+    youtube = {
+        "format": "worst[ext=mp4]",
+        "external_downloader": "aria2c",
+    }
 
 
 class YtdlpDownloader:
@@ -49,20 +63,26 @@ class YtdlpDownloader:
             title=info["title"],
         )
 
+    # FIXME: api must be same with download audio
     @classmethod
     async def download_video(cls, url: str) -> FSInputFile:
-        return await cls.__download_file(url, video_opts, "video.mp4")
+        opts = cls.__choose_video_opts(url)
+        return await cls.__download_file(url, opts, "video.mp4")
 
     @classmethod
     async def __download_audio_file(cls, url: str) -> FSInputFile:
-        opts = cls.__choose_opts(url)
+        opts = cls.__choose_audio_opts(url)
         file = await cls.__download_file(url, opts, "audio.m4a")
         return file
 
     @classmethod
     @async_wrap
-    def __download_file(cls, url: str, opts: dict, file_name: str) -> FSInputFile:
+    def __download_file(
+        cls, url: str, opts: DownloadOptions, file_name: str
+    ) -> FSInputFile:
         output_path = cls.__prepare_path() + "/" + file_name
+        opts = opts.value.copy()
+        print(opts)  # FIXME:
         opts["outtmpl"] = output_path
 
         with yt_dlp.YoutubeDL(opts) as ydl:
@@ -74,10 +94,17 @@ class YtdlpDownloader:
         return FSInputFile(output_path, file_name)
 
     @staticmethod
-    def __choose_opts(url: str) -> dict:
+    def __choose_audio_opts(url: str) -> DownloadOptions:
+        # FIXME: refactor it use filters
         if url.startswith("https://vk.com"):
-            return vkdl_opts
-        return ydl_opts
+            return AudioDownloadOptions.vk
+        return AudioDownloadOptions.youtube
+
+    @staticmethod
+    def __choose_video_opts(url: str) -> DownloadOptions:
+        if re.match(r"^https://(www\.)?youtube\.com/shorts", url):
+            return VideoDownloadOptions.youtube_shorts
+        return VideoDownloadOptions.youtube
 
     @staticmethod
     def __prepare_path() -> str:
