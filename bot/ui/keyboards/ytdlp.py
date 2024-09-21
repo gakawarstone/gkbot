a98d896383d1dc00c0da838cee02de65def4be3b
Gakawarstone <pinnekerv@gmail.com>
2024-09-21 18:13:41 +0200

    chore: remove unused code

--- bot/ui/keyboards/ytdlp.py
+++ bot/ui/keyboards/ytdlp.py
@@ -3,37 +3,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 class _Buttons:
-    delete = 'Убрать'
-    audio = 'Аудио'
-    video = 'Видео'
+    delete = "Убрать"
+    audio = "Аудио"
+    video = "Видео"
 
 
 class _Data:
-    delete = 'del'
-    mp3 = 'ytdlmp3'
-    mp4 = 'ytdlmp4'
+    delete = "del"
+    mp3 = "ytdlmp3"
+    mp4 = "ytdlmp4"
 
 
 class YtdlpMarkup:
     buttons = _Buttons
     data = _Data
-
-    @classmethod
-    def get_download_mp3_dialog(cls, user_id: int, message_id: int):
-        return InlineKeyboardBuilder(
-            [
-                [
-                    InlineKeyboardButton(
-                        text=_Buttons.mp3,
-                        callback_data=f'{_Data.mp3}:{user_id}:{message_id}'
-                    ),
-                    InlineKeyboardButton(
-                        text=_Buttons.delete,
-                        callback_data=_Data.delete
-                    )
-                ]
-            ]
-        ).as_markup()
 
     @classmethod
     def get_download_dialog(cls, user_id: int, message_id: int):
@@ -42,16 +25,15 @@ class YtdlpMarkup:
                 [
                     InlineKeyboardButton(
                         text=_Buttons.audio,
-                        callback_data=f'{_Data.mp3}:{user_id}:{message_id}'
+                        callback_data=f"{_Data.mp3}:{user_id}:{message_id}",
                     ),
                     InlineKeyboardButton(
                         text=_Buttons.video,
-                        callback_data=f'{_Data.mp4}:{user_id}:{message_id}'
+                        callback_data=f"{_Data.mp4}:{user_id}:{message_id}",
                     ),
                     InlineKeyboardButton(
-                        text=_Buttons.delete,
-                        callback_data=_Data.delete
-                    )
+                        text=_Buttons.delete, callback_data=_Data.delete
+                    ),
                 ]
             ]
         ).as_markup()
