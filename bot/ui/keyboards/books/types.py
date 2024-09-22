4751da9be549e282d87885aee660f64dcddb06ad
Gakawarstone <pinnekerv@gmail.com>
2024-09-22 13:48:47 +0200

    feat: update book ui

--- bot/ui/keyboards/books/types.py
+++ bot/ui/keyboards/books/types.py
@@ -10,42 +10,30 @@ class _BookProperty:
 
 
 class _BookProperties(Enum):
-    NAME = _BookProperty(
-        name_in_db='name',
-        spell_ru='Название',
-        event_code='ttl'
-    )
-    AUTHOR = _BookProperty(
-        name_in_db='author',
-        spell_ru='Автор',
-        event_code='aut'
-    )
+    NAME = _BookProperty(name_in_db="name", spell_ru="Название", event_code="ttl")
+    AUTHOR = _BookProperty(name_in_db="author", spell_ru="Автор", event_code="aut")
     PROGRESS = _BookProperty(
-        name_in_db='current_chapter',
-        spell_ru='Прогресс',
-        event_code='prg'
+        name_in_db="current_chapter", spell_ru="Прогресс", event_code="prg"
     )
     TOTAL_CHAPTERS = _BookProperty(
-        name_in_db='chapters_cnt',
-        spell_ru='Всего глав',
-        event_code='tlc'
+        name_in_db="chapters_cnt", spell_ru="Всего глав", event_code="tlc"
     )
 
 
 # NOTE enum instead of dataclass
 @dataclass
 class _Buttons:
-    add_new_book = '📘 Добавить книгу'
-    my_books = '📚 Мои книги'
-    update_book = '✏️ Изменить книгу'
-    delete_book = '❌ Удалить книгу'
-    exit = '🚪 Выйти'
+    add_new_book = "📘 Добавить"
+    my_books = "📚 Мои книги"
+    update_book = "✏️ Изменить"
+    delete_book = "❌ Удалить"
+    exit = "🚪 Выйти"
 
 
 @dataclass
 class _Events:
-    show = 'show'
-    increment = 'inc'
-    decrement = 'dec'
-    edit = 'edt'
-    delete = 'del'
+    show = "show"
+    increment = "inc"
+    decrement = "dec"
+    edit = "edt"
+    delete = "del"
