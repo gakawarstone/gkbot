9928d0acdc3852b474012edad0fb328bcf44ea2a
Gakawarstone <pinnekerv@gmail.com>
2025-02-24 18:43:46 +0100

    chore: format

--- bot/handlers/text/ytdlp/callback.py
+++ bot/handlers/text/ytdlp/callback.py
@@ -12,39 +12,35 @@ async def delete_download_markup(callback: CallbackQuery):
 
 
 async def download_audio(callback: CallbackQuery, bot: Bot):
-    request_from_user_id = int(callback.data.split(':')[1])
+    request_from_user_id = int(callback.data.split(":")[1])
     if callback.from_user.id != request_from_user_id:
         return
 
     await bot.delete_message(
-        chat_id=callback.message.chat.id,
-        message_id=callback.data.split(':')[2]
+        chat_id=callback.message.chat.id, message_id=callback.data.split(":")[2]
     )
     await callback.message.edit_reply_markup()
-    await callback.message.edit_text('Cкачиваю ' +
-                                     (url := callback.message.text))
+    await callback.message.edit_text("Cкачиваю " + (url := callback.message.text))
     audio = await YtdlpDownloader.download_audio(url)
     await callback.message.answer_audio(
         audio=audio.input_file,
         title=audio.title,
-        performer='GKBOT',
-        duration=audio.duration
+        performer="GKBOT",
+        duration=audio.duration,
     )
     await callback.message.delete()
 
 
 async def download_video(callback: CallbackQuery, bot: Bot):
-    request_from_user_id = int(callback.data.split(':')[1])
+    request_from_user_id = int(callback.data.split(":")[1])
     if callback.from_user.id != request_from_user_id:
         return
 
     await bot.delete_message(
-        chat_id=callback.message.chat.id,
-        message_id=callback.data.split(':')[2]
+        chat_id=callback.message.chat.id, message_id=callback.data.split(":")[2]
     )
     await callback.message.edit_reply_markup()
-    await callback.message.edit_text('Cкачиваю ' +
-                                     (url := callback.message.text))
+    await callback.message.edit_text("Cкачиваю " + (url := callback.message.text))
     video_file = await YtdlpDownloader.download_video(url)
     await callback.message.answer_video(video_file)
     await callback.message.delete()
@@ -52,14 +48,7 @@ async def download_video(callback: CallbackQuery, bot: Bot):
 
 def setup(r: Router):
     r.callback_query.register(
-        delete_download_markup,
-        F.data.startswith(YtdlpMarkup.data.delete)
+        delete_download_markup, F.data.startswith(YtdlpMarkup.data.delete)
     )
-    r.callback_query.register(
-        download_audio,
-        F.data.startswith(YtdlpMarkup.data.mp3)
-    )
-    r.callback_query.register(
-        download_video,
-        F.data.startswith(YtdlpMarkup.data.mp4)
-    )
+    r.callback_query.register(download_audio, F.data.startswith(YtdlpMarkup.data.mp3))
+    r.callback_query.register(download_video, F.data.startswith(YtdlpMarkup.data.mp4))
