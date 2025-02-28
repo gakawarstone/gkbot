55289e415a69869487117617afe2ef17617a0459
Gakawarstone <pinnekerv@gmail.com>
2025-02-28 15:41:01 +0100

    fix: youtube feed item views

--- bot/handlers/user/feed/views/youtube.py
+++ bot/handlers/user/feed/views/youtube.py
@@ -7,8 +7,8 @@ class YoutubeFeedItemView(BaseFeedItemView):
     async def _process_youtube_item(self, item: FeedItem):
         try:
             data = await YoutubeApiService.get_video_data(item.link)
+            await self._send_photo(
+                item, data.thumbnail_url, data.title, data.channel_title
+            )
         except UnavailableVideo:
             await self._send_item(item)
-            return
-
-        await self._send_photo(item, data.thumbnail_url, data.title, data.channel_title)
