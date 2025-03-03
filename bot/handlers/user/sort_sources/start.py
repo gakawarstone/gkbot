957178e878242463a35040771619df260535a69b
Gakawarstone <pinnekerv@gmail.com>
2025-03-03 14:00:43 +0100

    feat(ui): add sort documents image and update handler to send photo
    Add new static image file sort_documents.png for visual guidance
    
    Include sort_documents image in Static service class
    
    Modify sort_sources initiation handler to delete original message and send instructional photo with caption
    
    Update message response from text to photo format for better user experience

--- bot/handlers/user/sort_sources/start.py
+++ bot/handlers/user/sort_sources/start.py
@@ -1,11 +1,15 @@
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
+from services.static import Images
 from ._states import FSM
 
 
 async def init(message: Message, state: FSMContext):
+    await message.delete()
     await state.set_state(FSM.sort_file)
     text = "Вы включили сортировщик призваный избавить вас от ручной работы "
     text += "<b>Отправьте .txt файл с вашими источниками</b>"
-    await message.answer(text)
+    await message.answer_photo(
+        await Images.sort_documents.as_input_file(), caption=text
+    )
--- bot/services/static.py
+++ bot/services/static.py
@@ -54,6 +54,7 @@ class _StaticFile:
 class Images:
     book_shelf = _StaticFile("./static/book_shelf.png")
     road_greet = _StaticFile("./static/road_greet.png")
+    sort_documents = _StaticFile("./static/sort_documents.png")
 
 
 class TextFiles:
