800935568c94e88fd25b523cb2970941746757fc
gakawarstone <pinnekerv@gmail.com>
2025-05-31 09:46:26 +0200

    feat: add image generation

--- bot/handlers/prompt/__init__.py
+++ bot/handlers/prompt/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Router
 
 from . import gpt
 from . import wait
+from . import generate_image
 
 
 def setup(r: Router):
     gpt.setup(r)
     wait.setup(r)
+    generate_image.setup(r)
