7db6d065ddcedff067c302a7e8d425567fd57016
Gakawarstone <pinnekerv@gmail.com>
2025-05-31 11:59:05 +0200

    fix: prompt commands is not long message

--- bot/filters/long_message.py
+++ bot/filters/long_message.py
@@ -1,9 +1,12 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
+from pydantic_core.core_schema import model_ser_schema
 
 
 class LongMessageFilter(BaseFilter):
     async def __call__(self, message: Message) -> bool:
+        if message.text.startswith("/"):
+            return False
         if not message.text:
             return False
         return len(message.text) > 200
