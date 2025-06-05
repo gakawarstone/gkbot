6855a6a722fc927db33eba63300be660c083ca01
Gakawarstone <pinnekerv@gmail.com>
2025-06-05 12:08:45 +0200

    feat: generate 4 images

--- bot/handlers/prompt/generate_image.py
+++ bot/handlers/prompt/generate_image.py
@@ -1,18 +1,49 @@
+import asyncio
 import random
 
 from aiogram import Router
-from aiogram.types import Message, BufferedInputFile
+from aiogram.types import (
+    Message,
+    BufferedInputFile,
+    InputMediaPhoto,
+    InputMediaAudio,
+    InputMediaDocument,
+    InputMediaVideo,
+)
 
 from filters.command import CommandWithPrompt
 from services.agents.image_prompt_enhancer import ImagePromptEnhancer
 from services.http import HttpService
 
+_MEDIA_GROUP = list[
+    InputMediaAudio | InputMediaDocument | InputMediaPhoto | InputMediaVideo
+]
+
 
 async def generate_image(m: Message):
     await m.delete()
+
+    if not m.text:
+        return
+
     prompt = " ".join(m.text.split(" ")[1:])
     prompt = await ImagePromptEnhancer.enhance(prompt)
 
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        for _ in range(4):
+            img_url = _generate_url(prompt)
+            tasks.append(tg.create_task(HttpService.get(img_url)))
+
+    media_group: _MEDIA_GROUP = [
+        InputMediaPhoto(media=BufferedInputFile(task.result(), "image.jpg"))
+        for task in tasks
+    ]
+
+    await m.answer_media_group(media_group)
+
+
+def _generate_url(prompt: str) -> str:
     img_url = f"https://image.pollinations.ai/prompt/{prompt}".replace(" ", "+").strip()
     img_url += f"?width=1024"
     img_url += f"&height=1024"
@@ -22,10 +53,7 @@ async def generate_image(m: Message):
     img_url += "&enhance=false"
     img_url += "&safe=false"
     img_url += f"&seed={random.randint(10**9, 10**10 - 1)}"
-
-    print(img_url)
-    content = await HttpService.get(img_url)
-    await m.answer_photo(BufferedInputFile(content, "image.jpg"))
+    return img_url
 
 
 def setup(r: Router):
