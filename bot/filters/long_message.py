ba9a55cbe47b4e9461d7f96af6a7d718fee44f4f
gakawarstone <pinnekerv@gmail.com>
2025-06-23 13:58:21 +0200

    fix: filter

--- bot/filters/long_message.py
+++ bot/filters/long_message.py
@@ -1,10 +1,11 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
-from pydantic_core.core_schema import model_ser_schema
 
 
 class LongMessageFilter(BaseFilter):
     async def __call__(self, message: Message) -> bool:
+        if not hasattr(message, "text") or not message.text:
+            return False
         if message.text.startswith("/"):
             return False
         if not message.text:
