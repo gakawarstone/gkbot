5a2a81ee908721ef1ba39bc09f9eff8ed19ca982
Gakawarstone <pinnekerv@gmail.com>
2025-07-11 10:55:05 +0200

    fix: move db on_startup from models

--- bot/configs/startup.py
+++ bot/configs/startup.py
@@ -1,8 +1,10 @@
+from models import setup
 from . import db
 from services.schedule import Schedule
 
 
 TASKS_ON_STARTUP_ASYNC = [
+    setup(DB_URL, MODELS),
     db.on_startup(),
     Schedule.on_startup(),
 ]
