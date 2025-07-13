ed58be1da629595f6fc6dd9cc735527e2bf9327f
Gennadiy Ivashchenko <gena9902@gmail.com>
2025-07-13 19:47:29 +0500

    fix: notifier not only for admins

--- bot/core/notifier.py
+++ bot/core/notifier.py
@@ -1,6 +1,8 @@
 from aiogram import Bot
 from aiogram.exceptions import TelegramForbiddenError
 
+from configs.admins import ADMINS
+
 
 class Notifier:
     @classmethod
@@ -9,7 +11,12 @@ class Notifier:
 
     @classmethod
     async def notify(cls, chat_id: int, text: str) -> None:
+        if chat_id in ADMINS:
+            type_chat_id = "admin"
+        else:
+            type_chat_id = "user"
+
         try:
             await cls.__bot.send_message(chat_id, text)
         except TelegramForbiddenError:
-            print(f"admin with id: {chat_id} blocked bot")
+            print(f"{type_chat_id} with id: {chat_id} blocked bot")
