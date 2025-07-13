ea40200e22ab52b0111bd77aa53cf77824df07aa
Gakawarstone <pinnekerv@gmail.com>
2025-07-13 10:13:34 +0200

    chore: update test

--- bot/tests/handlers/test_tiktok.py
+++ bot/tests/handlers/test_tiktok.py
@@ -14,20 +14,24 @@ class MockedHandler(TikTokVideoHandler):
         return Bot()
 
 
+async def _handle_url(url: str):
+    await MockedHandler(event=Event(text=url, breakpoint_at_delete=False)).handle()
+
+
 @integration_test
 @pytest.mark.asyncio(loop_scope="session")
 async def test_download_video():
-    await MockedHandler(
-        event=Event(text="https://vm.tiktok.com/ZMBwPoDGc/", breakpoint_at_delete=False)
-    ).handle()
+    await _handle_url("https://vm.tiktok.com/ZMBwPoDGc/")
 
 
 @integration_test
 @pytest.mark.asyncio(loop_scope="session")
 async def test_download_slideshow():
-    await MockedHandler(
-        event=Event(text="https://vt.tiktok.com/ZShECspQn/", breakpoint_at_delete=False)
-    ).handle()
-    await MockedHandler(
-        event=Event(text="https://vt.tiktok.com/ZSk1SHU9U/", breakpoint_at_delete=False)
-    ).handle()
+    urls = [
+        # "https://vt.tiktok.com/ZShECspQn/",
+        # "https://vt.tiktok.com/ZSk1SHU9U/",
+        # "https://vt.tiktok.com/ZSkhnmgaB/",
+        "https://vt.tiktok.com/ZSkMFNRau/",
+    ]
+    for url in urls:
+        await _handle_url(url)
