80a6b53f01f22769a8be479ccec52673100ed762
Gennadiy Ivashchenko <gena9902@gmail.com>
2025-07-14 12:43:07 +0500

    chore: user still user

--- bot/core/notifier.py
+++ bot/core/notifier.py
@@ -1,8 +1,6 @@
 from aiogram import Bot
 from aiogram.exceptions import TelegramForbiddenError
 
-from configs.admins import ADMINS
-
 
 class Notifier:
     @classmethod
@@ -11,12 +9,7 @@ class Notifier:
 
     @classmethod
     async def notify(cls, chat_id: int, text: str) -> None:
-        if chat_id in ADMINS:
-            type_chat_id = "admin"
-        else:
-            type_chat_id = "user"
-
         try:
             await cls.__bot.send_message(chat_id, text)
         except TelegramForbiddenError:
-            print(f"{type_chat_id} with id: {chat_id} blocked bot")
+            print(f"user with id: {chat_id} blocked bot")
