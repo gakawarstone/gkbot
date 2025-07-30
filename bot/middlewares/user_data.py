b0a32bfb00f5a6d78619148caa383a6083cb9776
gakawarstone <pinnekerv@gmail.com>
2025-07-30 16:31:21 +0200

    chore: make middleware type safe

--- bot/middlewares/user_data.py
+++ bot/middlewares/user_data.py
@@ -1,17 +1,26 @@
 from typing import Callable, Dict, Awaitable, Any
 
-from aiogram.types import Message
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
+from aiogram.types import TelegramObject
 
 
 class UserDataMiddleware(BaseMiddleware):
-    __data = {}
+    __data: Dict[int, Dict[str, Any]] = {}
 
     async def __call__(
-            self, handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-            event: Message, data: Dict[str, Any]) -> Any:
-        if event.from_user.id not in self.__data:
-            self.__data[event.from_user.id] = {}
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        user = getattr(event, "from_user", None)
+        user_id = user.id if user else None
 
-        data['data'] = self.__data[event.from_user.id]
+        if user_id is None:
+            raise ValueError("Event does not contain a user.")
+
+        if user_id not in self.__data:
+            self.__data[user_id] = {}
+        data["data"] = self.__data[user_id]
+
         await handler(event, data)
