0be21188aadf761bf1c75f5150d751d11a4cbd60
Gakawarstone <pinnekerv@gmail.com>
2025-07-31 15:42:45 +0200

    fix: ytdlp extractor

--- bot/services/tiktok/extractor/yt_dlp.py
+++ bot/services/tiktok/extractor/yt_dlp.py
@@ -1,6 +1,6 @@
 import os
 
-from aiogram.types import InputFile
+from aiogram.types import InputFile, FSInputFile
 from yt_dlp import DownloadError
 
 from configs.services.cache_dir import CACHE_DIR_PATH
@@ -29,8 +29,12 @@ class YtDlp(BaseExtractor):
 
     async def get_video_file_url(self, url: str) -> str:
         try:
-            fs_input_file = await self._get_video_input_file(url)
-            video_path = "/".join(fs_input_file.path.split("/")[-3:])
+            video_file_info = await YtdlpDownloader.download_video(url)
+            fs_input_file = video_file_info.input_file
+            if not isinstance(fs_input_file, FSInputFile):
+                raise ValueError("Input file is not an FSInputFile instance")
+
+            video_path = "/".join(str(fs_input_file.path).split("/")[-3:])
             url_path = os.path.expanduser(CACHE_DIR_PATH + "/serveo_url")
             serveo_url = open(url_path).read()
             return serveo_url + "/" + video_path
