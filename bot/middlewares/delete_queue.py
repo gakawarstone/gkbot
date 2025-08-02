e9376e9c32ee9985e371dd1a2b1c463b20b270dd
Gakawarstone <pinnekerv@gmail.com>
2025-08-02 14:39:43 +0200

    fix: middlewares

--- bot/middlewares/delete_queue.py
+++ bot/middlewares/delete_queue.py
@@ -1,17 +1,19 @@
 from typing import Awaitable, Callable, Dict, Any
 
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import TelegramObject
 
 
 class DeleteQueueMiddleware(BaseMiddleware):
     async def __call__(
-            self, handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-            event: Message, data: Dict[str, Any]
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
     ) -> Any:
-        if 'delete_queue' not in data['data'].keys():
-            data['data']['delete_queue'] = []
-        if len(messages := data['data']['delete_queue']) > 0:
+        if "delete_queue" not in data["data"].keys():
+            data["data"]["delete_queue"] = []
+        if len(messages := data["data"]["delete_queue"]) > 0:
             [await m.delete() for m in messages]
-            data['data']['delete_queue'] = []
+            data["data"]["delete_queue"] = []
         await handler(event, data)
--- bot/middlewares/not_enough_rights.py
+++ bot/middlewares/not_enough_rights.py
@@ -1,19 +1,23 @@
 from typing import Awaitable, Callable, Dict, Any
 
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import TelegramObject, Message
 from aiogram.exceptions import TelegramBadRequest
 
 
 class NotEnoughRightsMiddleware(BaseMiddleware):
     async def __call__(
-            self, handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-            event: Message, data: Dict[str, Any]
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
     ) -> Any:
         try:
             await handler(event, data)
         except TelegramBadRequest as e:
-            if e.message == 'Bad Request: message can\'t be deleted':
-                await event.answer('Bot should can delete messages')
+            if e.message == "Bad Request: message can't be deleted" and isinstance(
+                event, Message
+            ):
+                await event.answer("Bot should can delete messages")
             else:
                 raise e
