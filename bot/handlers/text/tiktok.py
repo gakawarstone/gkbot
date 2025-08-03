7c6b44b7c530d60ae3a339a7bacd12dc24bb6bea
Gakawarstone <pinnekerv@gmail.com>
2025-08-03 14:04:50 +0200

    fix: tiktok handler

--- bot/handlers/text/tiktok.py
+++ bot/handlers/text/tiktok.py
@@ -16,22 +16,27 @@ from filters.tiktok import TikTokVideoLink
 class TikTokVideoHandler(BaseHandler):
     async def handle(self) -> Any:
         await self.event.delete()
-        status_message = await self.event.answer("Скачиваю " + self._tiktok_link)
+
+        link = self._tiktok_link
+        status_message = await self.event.answer(f"Скачиваю {link}")
         await self.bot.send_chat_action(self.event.chat.id, "upload_video")
+
         try:
-            await self._send_tiktok_video(self._tiktok_link)
+            await self._send_tiktok_video(link)
             await status_message.delete()
         except TikTokInvalidUrl:
-            status_text = "Неправильная ссылка " + self._tiktok_link
-            await status_message.edit_text(status_text)
+            await status_message.edit_text(f"Неправильная ссылка {link}")
         except TikTokInfoExtractionFailed:
-            status_text = "Не получилось скачать " + self._tiktok_link
-        else:
-            return
-        await status_message.edit_text(status_text)
+            await status_message.edit_text(f"Не получилось скачать {link}")
 
     async def _send_tiktok_video(self, link: str) -> None:
-        caption = f'<b>{self.event.from_user.username}</b> ➤ <a href="{link.split("?")[0]}">TikTok</a>'
+        if not self.event.from_user:
+            raise ValueError("Event does not have a from_user")
+        from_user = self.event.from_user
+
+        username = from_user.username if from_user.username else "user"
+        caption = f'<b>{username}</b> ➤ <a href="{link.split("?")[0]}">TikTok</a>'
+
         try:
             video_url = await TikTokService.get_video_url(link)
             await self.event.answer_video(video_url, caption=caption)
@@ -41,6 +46,8 @@ class TikTokVideoHandler(BaseHandler):
 
     @property
     def _tiktok_link(self) -> str:
+        if not self.event.text:
+            raise ValueError("TikTok link text is missing")
         return self.event.text
 
 
