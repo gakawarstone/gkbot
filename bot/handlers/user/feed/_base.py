3f37db6e53e6e68b41cd6f598c23388870894c27
Gakawarstone <pinnekerv@gmail.com>
2025-08-03 20:51:07 +0200

    fix: gkfeed base handler

--- bot/handlers/user/feed/_base.py
+++ bot/handlers/user/feed/_base.py
@@ -6,10 +6,18 @@ from services.gkfeed import GkfeedService, FeedItem
 from ui.keyboards.feed import FeedMarkup
 
 
+def _get_gkfeed() -> GkfeedService:
+    if GKFEED_USER is None or GKFEED_PASSWORD is None:
+        raise ValueError("GKFEED_USER and GKFEED_PASSWORD must be set")
+    return GkfeedService(GKFEED_USER, GKFEED_PASSWORD)
+
+
 class BaseHandler(_BaseHandler):
-    _gkfeed = GkfeedService(GKFEED_USER, GKFEED_PASSWORD)
+    _gkfeed = _get_gkfeed()
 
     async def _send_item(self, item: FeedItem):
+        if self.event.from_user is None:
+            raise ValueError("from_user is required to send a message")
         await self.bot.send_message(
             self.event.from_user.id,
             f'<a href="{item.link}">Link</a>',
@@ -18,5 +26,7 @@ class BaseHandler(_BaseHandler):
 
     async def answer(self, *args, **kwargs):
         if isinstance(self.event, CallbackQuery):
+            if self.event.message is None:
+                raise ValueError("message is required for CallbackQuery.answer")
             return await self.event.message.answer(*args, **kwargs)
         return await self.event.answer(*args, **kwargs)
