45ff6afb4854ae97f662487c1c2df814691ea105
Gakawarstone <pinnekerv@gmail.com>
2025-08-03 21:51:51 +0200

    chore: piokok view deprecated

--- bot/handlers/user/feed/views/piokok/callbacks.py
+++ bot/handlers/user/feed/views/piokok/callbacks.py
@@ -4,6 +4,7 @@ from services.http import HttpService
 from extensions.handlers.message.http import HttpExtension
 from extensions.handlers.callback.carousel import CarouselWidgetEventHandlerExtension
 from ui.keyboards.feed.piokok import PiokokFeedItemMarkup
+from bs4 import Tag
 from ..._base import BaseHandler
 
 
@@ -18,9 +19,9 @@ class PiokokCarouselWidgetEventHandler(
         item_id = int(await self._callback_data())
         item = await self._gkfeed.get_item_by_item_id(int(item_id))
         soup = await self._get_soup(item.link)
-        return [
-            InputMediaPhoto(
-                media=URLInputFile(await HttpService.get_redirected_url(pic.a["href"]))
-            )
-            for pic in soup.find_all(class_="pic")
-        ]
+        photos: list[InputMediaPhoto] = []
+        for pic in soup.find_all(class_="pic"):
+            if isinstance(pic, Tag) and pic.a and pic.a.get("href"):
+                redirected = await HttpService.get_redirected_url(str(pic.a.get("href")))
+                photos.append(InputMediaPhoto(media=URLInputFile(redirected)))
+        return photos
--- bot/handlers/user/feed/views/piokok/view.py
+++ bot/handlers/user/feed/views/piokok/view.py
@@ -4,19 +4,27 @@ from services.gkfeed import FeedItem
 from services.http import HttpService
 from extensions.handlers.message.http import HttpExtension
 from ui.keyboards.feed.piokok import PiokokFeedItemMarkup
+from bs4 import Tag
 from ..._base import BaseHandler
 from ..video import VideoFeedItemView
 
 
+# NOTE: deprecated piokok not working anymore
 class PiokokFeedItemView(VideoFeedItemView, BaseHandler, HttpExtension):
     async def _process_piokok_item(self, item: FeedItem):
         soup = await self._get_soup(item.link)
 
-        photos = [
-            await HttpService.get_redirected_url(pic.a["href"])
-            for pic in soup.find_all(class_="pic")
-        ]
-        videos = [vid.a["href"] for vid in soup.find_all(class_="video_img")]
+        photos = []
+        for pic in soup.find_all(class_="pic"):
+            if isinstance(pic, Tag) and pic.a and pic.a.get("href"):
+                photos.append(
+                    await HttpService.get_redirected_url(str(pic.a.get("href")))
+                )
+
+        videos = []
+        for vid in soup.find_all(class_="video_img"):
+            if isinstance(vid, Tag) and vid.a and vid.a.get("href"):
+                videos.append(str(vid.a.get("href")))
 
         if photos:
             await self.__send_piokok_item_as_carousel(item, photos[0], len(photos))
