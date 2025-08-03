b04328ce72785eb86cef1c81f231fe5ab3de42d9
Gakawarstone <pinnekerv@gmail.com>
2025-08-03 21:23:47 +0200

    fix: road settings

--- bot/handlers/user/road/settings/edit/get_new_setting_value.py
+++ bot/handlers/user/road/settings/edit/get_new_setting_value.py
@@ -8,19 +8,31 @@ from .._context import RoadSettingsContextManager
 
 
 class InvalidTimeInput(Exception):
-    '''invalid time string'''
+    """invalid time string"""
 
 
 class GetNewSettingValueHandler(RoadSettingsContextManager):
     async def handle(self) -> Any:
         await self.state.set_state(FSM.finish)
         await self.event.delete()
+
+        if not self.event.text:
+            raise ValueError("event.text is None")
+
         value = self._validate_time(self.event.text)
-        user_id = self.event.from_user.id
         self.set(self.props.setting_new_value, value)
+
+        user_id = self.event.from_user.id
+        setting_name = self.ctx.setting_name
+
+        if not setting_name:
+            raise ValueError("setting_name is None")
+
         await RoadSettingsRepository.update_user_setting(
-            user_id, self.ctx.setting_name, self.ctx.setting_new_value)
-        await self.event.answer('Изменено')
+            user_id, setting_name, self.ctx.setting_new_value
+        )
+
+        await self.event.answer("Изменено")
         await self.state.set_state(FSM.menu)
 
     # FIX: from reminder move to contrib
