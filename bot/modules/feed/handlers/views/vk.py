b6e272818e08ca58e9cc9861ec2bad1258b51c1d
Gakawarstone <pinnekerv@gmail.com>
2025-08-05 21:53:34 +0200

    fix: vk text item view

--- bot/modules/feed/handlers/views/vk.py
+++ bot/modules/feed/handlers/views/vk.py
@@ -1,7 +1,8 @@
-from bs4 import Tag
+from bs4 import Tag, BeautifulSoup
 
 from services.gkfeed import FeedItem
 from extensions.handlers.message.http import HttpExtension
+from ...ui.keyboards import FeedMarkup
 from . import BaseFeedItemView
 
 
@@ -11,11 +12,11 @@ class VKFeedItemView(BaseFeedItemView, HttpExtension):
 
         meta_tag = soup.find("meta", attrs={"property": "og:image"})
         if not isinstance(meta_tag, Tag):
-            return await self._send_item(item)
+            return await self._send_text_item(item, soup)
 
         media_url = meta_tag.get("content")
         if not media_url:
-            return await self._send_item(item)
+            return await self._send_text_item(item, soup)
 
         title_tag = soup.find("title")
         title = title_tag.text if isinstance(title_tag, Tag) else ""
@@ -23,3 +24,26 @@ class VKFeedItemView(BaseFeedItemView, HttpExtension):
         await self._send_photo(
             item, str(media_url), title.split(" | ")[0], title.split(" | ")[1]
         )
+
+    async def _send_text_item(self, item: FeedItem, soup: BeautifulSoup):
+        soup = await self._get_soup(item.link)
+
+        description_tag = soup.find("meta", attrs={"property": "og:description"})
+        if not description_tag or not isinstance(description_tag, Tag):
+            raise ValueError("Description meta tag not found")
+
+        description = description_tag.get("content")
+        if not description or not isinstance(description, str):
+            raise ValueError("Description content not found")
+
+        await self.bot.send_message(
+            self.event.from_user.id,
+            f"<b>{description.replace('<br>', '\n')}</b>\n\n<a href='{item.link}'>{self._get_channel_name(soup)}</a>",
+            reply_markup=FeedMarkup.get_item_markup(item.id, item.feed_id),
+            disable_web_page_preview=True,
+        )
+
+    def _get_channel_name(self, soup: BeautifulSoup) -> str:
+        title_tag = soup.find("title")
+        title = title_tag.text if isinstance(title_tag, Tag) else ""
+        return title.split(" | ")[1]
