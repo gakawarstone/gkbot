25e1936eee76097557cd5409d8296438d5733e80
gakawarstone <pinnekerv@gmail.com>
2025-08-05 11:08:19 +0200

    fix: settings

--- bot/handlers/user/road/settings/__init__.py
+++ bot/handlers/user/road/settings/__init__.py
@@ -1,13 +1,10 @@
 from aiogram import F, Router
 from aiogram.filters import StateFilter
-from aiogram.types import CallbackQuery
 
 from ..states import FSM
 from .callback import EditSettingsHandler
 from .edit.get_new_setting_value import GetNewSettingValueHandler
 from ui.keyboards.pomodoro import PomodoroSettingsMarkup
-
-F: CallbackQuery = F
 
 
 def setup(r: Router):
