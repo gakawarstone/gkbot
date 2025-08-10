95afe61e223c2cdf17db7e74d6c697f21c94a1a3
Gakawarstone <pinnekerv@gmail.com>
2025-08-10 20:19:27 +0200

    fix: yt video download

--- bot/services/ytdlp/_options.py
+++ bot/services/ytdlp/_options.py
@@ -26,7 +26,14 @@ class VideoDownloadOptions(DownloadOptions):
     }
 
     youtube = {
-        "format": "136+ba[ext=m4a]",
+        "format": "bv[ext=mp4][height=720]+ba[ext=m4a]",
+        "external_downloader": "aria2c",
+        "external_downloader_args": [
+            "-x",
+            "16",
+            "-k",
+            "1M",
+        ],
         "postprocessors": [
             {"key": "SponsorBlock", "categories": ["sponsor"]},
             {"key": "ModifyChapters", "remove_sponsor_segments": ["sponsor"]},
