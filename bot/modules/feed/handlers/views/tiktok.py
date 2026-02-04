02399302507332714af0763a3f45e54e67cba213
Gakawarstone <pinnekerv@gmail.com>
2026-02-04 14:18:28 +0100

    fix: heigth issues with tiktok feed item view

--- bot/modules/feed/handlers/views/tiktok.py
+++ bot/modules/feed/handlers/views/tiktok.py
@@ -1,5 +1,8 @@
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.types import InputFile
+
 from services.gkfeed import FeedItem
-from services.tiktok import TikTokService
+from services.tiktok import TikTokService, TikTokVideoUrlExtractionFailed
 from . import BaseFeedItemView
 from .video import VideoFeedItemView
 from ...ui.keyboards import FeedMarkup
@@ -7,7 +10,14 @@ from ...ui.keyboards import FeedMarkup
 
 class TikTokFeedItemView(VideoFeedItemView, BaseFeedItemView):
     async def _process_tiktok_item(self, item: FeedItem):
-        video = await TikTokService.get_video_as_input_file(item.link)
+        try:
+            video = await TikTokService.get_video_url(item.link)
+            await self._send_video_item(video, item)
+        except (TelegramBadRequest, TikTokVideoUrlExtractionFailed):
+            video = await TikTokService.get_video_as_input_file(item.link)
+            await self._send_video_item(video, item)
+
+    async def _send_video_item(self, video: str | InputFile, item: FeedItem):
         await self.answer_video(
             video,
             caption=f'<a href="{item.link}">{item.link.split("@")[1].split("/")[0]}</a>',
