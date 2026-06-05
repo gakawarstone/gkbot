1a89d771968f3a0c8e3004b3d0a28585d34efc46
Gakawarstone <pinnekerv@gmail.com>
2026-06-05 21:56:21 +0200

    chore: remove proxytok

--- bot/services/tiktok/extractor/__init__.py
+++ bot/services/tiktok/extractor/__init__.py
@@ -4,7 +4,6 @@ from ..exceptions import TikTokInfoExtractionFailed
 from ._base import BaseExtractor
 from .exceptions import SourceInfoExtractFailed
 
-# from .proxytok import ProxyTok
 from .api import ApiExtractor
 from .snaptik import Snaptik
 from .mdown import Mdown
@@ -16,22 +15,11 @@ from .yt_dlp import YtDlp
 _EXTRACTORS: list[BaseExtractor] = [
     YtDlp(),
     ApiExtractor("https://api.douyin.wtf"),
-    # ProxyTok("https://proxitok.pabloferreiro.es"),
     TTDownloader(),
-    # ProxyTok("https://tok.habedieeh.re"),
-    # ProxyTok("https://proxitok.belloworld.it"),
-    # ProxyTok("https://tt.vern.cc"),
     ApiExtractor("http://ttapi.gws.freemyip.com"),
-    # ProxyTok("http://tt.gws.freemyip.com"),
     Snaptik(),
     Mdown(),
     Tikdown(),
-    # ProxyTok("https://tok.adminforge.de"),
-    # ProxyTok("https://proxitok.esmailelbob.xyz"),
-    # ProxyTok("https://proxitok.pussthecat.org"),
-    # ProxyTok("https://cringe.whatever.social"),
-    # ProxyTok("https://tok.artemislena.eu"),
-    # ProxyTok("https://tik.hostux.net"),
 ]
 
 
