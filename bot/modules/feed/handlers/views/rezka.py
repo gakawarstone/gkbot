0a684c090d88c95f781c549a02bda5835f59e8e1
Gakawarstone <pinnekerv@gmail.com>
2026-07-07 22:02:44 +0200

    fix: rezka feed item view

--- bot/modules/feed/handlers/views/rezka.py
+++ bot/modules/feed/handlers/views/rezka.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlsplit, urlunsplit
+
 from services.gkfeed import FeedItem
 from extensions.handlers.message.http import HttpExtension
 from bs4 import Tag
@@ -6,7 +8,9 @@ from . import BaseFeedItemView
 
 class RezkaFeedItemView(BaseFeedItemView, HttpExtension):
     async def _process_rezka_item(self, item: FeedItem):
-        soup = await self._get_soup(item.link)
+        item_url = urlsplit(item.link)
+        preview_url = urlunsplit(item_url._replace(netloc="rezka.ag"))
+        soup = await self._get_soup(preview_url)
 
         meta_tag = soup.find("meta", attrs={"property": "og:image"})
         if not isinstance(meta_tag, Tag):
