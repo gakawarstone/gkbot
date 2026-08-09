b57f8d28b6e9121ffcf1b4e1c424d68603a55a5d
Gakawarstone <pinnekerv@gmail.com>
2026-08-09 22:05:41 +0200

    fix: tiktok download

--- bot/services/tiktok/__init__.py
+++ bot/services/tiktok/__init__.py
@@ -1,9 +1,11 @@
 from subprocess import SubprocessError
 
-from aiogram.types import BufferedInputFile, FSInputFile
+from aiogram.types import FSInputFile
 
-from services.http import HttpService
+from services.cache_dir import CacheDir
 from services.ffmpeg import FfmpegService
+from services.http import HttpService
+
 from .exceptions import TikTokInfoExtractionFailed, TikTokVideoUrlExtractionFailed
 from .extractor import TikTokInfoExtractor
 from .types import InfoVideoTikTok, TikTokVideo
@@ -18,28 +20,21 @@ class TikTokService:
 
     @classmethod
     async def get_video(cls, url: str) -> TikTokVideo:
-        info = await cls.get_video_info(url)
+        if not (info := await TikTokInfoExtractor.get_video_info(url)):
+            raise TikTokInfoExtractionFailed(url)
 
-        if not (input_file := info.video_input_file):
-            input_file = BufferedInputFile(
-                file=await cls.__get_video_file_from_info(info),
-                filename="video.mp4",
-            )
+        if info.video_input_file is None:
+            info.video_input_file = await cls.__create_cached_input_file(info)
+
+        await cls.__fill_missing_video_metadata(info)
 
         return TikTokVideo(
             url=info.video_url,
-            input_file=input_file,
+            input_file=info.video_input_file,
             height=info.height,
             width=info.width,
             duration=info.duration,
         )
-
-    @classmethod
-    async def get_video_info(cls, url: str) -> InfoVideoTikTok:
-        if not (info := await TikTokInfoExtractor.get_video_info(url)):
-            raise TikTokInfoExtractionFailed(url)
-        await cls.__fill_missing_video_metadata(info)
-        return info
 
     @classmethod
     async def __fill_missing_video_metadata(cls, info: InfoVideoTikTok) -> None:
@@ -51,14 +46,16 @@ class TikTokService:
             info.height = FfmpegService.SLIDESHOW_HEIGHT
             info.width = FfmpegService.SLIDESHOW_WIDTH
 
-        if not isinstance(info.video_input_file, FSInputFile):
-            return
-        source = str(info.video_input_file.path)
-
         try:
-            metadata = await FfmpegService.get_video_metadata(source)
+            if isinstance(info.video_input_file, FSInputFile):
+                metadata = await FfmpegService.get_video_metadata(
+                    str(info.video_input_file.path)
+                )
+            else:
+                return
         except SubprocessError:
             return
+
         if not info.height:
             info.height = metadata.get("height")
         if not info.width:
@@ -67,7 +64,15 @@ class TikTokService:
             info.duration = metadata.get("duration")
 
     @classmethod
-    async def __get_video_file_from_info(cls, info: InfoVideoTikTok) -> bytes:
+    async def __create_cached_input_file(cls, info: InfoVideoTikTok) -> FSInputFile:
+        video = await cls.__resolve_video_bytes(info)
+        cache_dir = CacheDir()
+        cache_dir.save_file("video.mp4", video)
+        await cache_dir.delete_after(minutes=5)
+        return FSInputFile(cache_dir.get_file_path("video.mp4"))
+
+    @classmethod
+    async def __resolve_video_bytes(cls, info: InfoVideoTikTok) -> bytes:
         if video_url := info.video_url:
             return await HttpService.get(video_url)
         if info.images_urls:
